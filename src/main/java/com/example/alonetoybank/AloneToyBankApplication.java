c80c890cd2faf6b8ab3ef5133ce78b570dc067a2
choiseon <com1368@naver.com>
2023-02-21 17:56:09 +0900

    Initial commit

---
+++ src/main/java/com/example/alonetoybank/AloneToyBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.alonetoybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloneToyBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AloneToyBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alonetoybank/AloneToyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alonetoybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AloneToyBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
