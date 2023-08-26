e4b2ca90f717a61af548fbf35ecb5fe0928c1592
choiseonkang <com1368@naver.com>
2023-08-26 11:37:13 +0900

    feat : 상품 등록 인수 테스트 작성

---
+++ bank-api/src/test/java/com/example/bankapi/product/acceptance/ProductAcceptanceTest.java
@@ -0,0 +1,44 @@
+package com.example.bankapi.product.acceptance;
+
+import com.example.bankapi.AcceptanceTest;
+import com.example.bankapi.member.acceptance.MemberSteps;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProductAcceptanceTest extends AcceptanceTest {
+
+    @BeforeEach
+    void setup() {
+        MemberSteps.회원_가입_요청("홍길동");
+    }
+
+    @DisplayName("여신 상품을 등록한다.")
+    @Test
+    void createLoan() {
+        // when
+        String token = MemberSteps.로그인_되어_있음("홍길동");
+
+        ExtractableResponse<Response> response = ProductSteps.상품_등록("대출", "loan", token);
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @DisplayName("수신 상품을 등록한다.")
+    @Test
+    void createDeposit() {
+        // when
+        String token = MemberSteps.로그인_되어_있음("홍길동");
+
+        ExtractableResponse<Response> response = ProductSteps.상품_등록("입출금", "deposit", token);
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+}
---
+++ bank-api/src/test/java/com/example/bankapi/product/acceptance/ProductSteps.java
@@ -0,0 +1,24 @@
+package com.example.bankapi.product.acceptance;
+
+import com.example.bankapi.AcceptanceTestSteps;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProductSteps extends AcceptanceTestSteps {
+
+    public static ExtractableResponse<Response> 상품_등록(String name, String type, String token) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+        params.put("type", type);
+
+        return given(token)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post("/api/v1/products")
+                .then().log().all().extract();
+    }
+}
